b1e2cb4fc2742483470090d74889acfc334da648
username <email@email.com>
2016-03-03 14:43:51 +0200

    adding models, api controllers and startup

---
+++ src/HelloWorld_ASPNetMVC5_AJS/API/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using HelloWorld_ASPNetMVC5_AJS.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorld_ASPNetMVC5_AJS.API
+{
+  [Route("api/[controller]")]
+  public class MoviesController : Controller
+  {
+    [HttpGet]
+    public IEnumerable<Movie> Get()
+    {
+      return new List<Movie> {
+                new Movie {Id=1, Title="Star Wars", Director="Lucas"},
+                new Movie {Id=2, Title="King Kong", Director="Jackson"},
+                new Movie {Id=3, Title="Memento", Director="Nolan"}
+            };
+    }
+
+    //// GET: api/values
+    //[HttpGet]
+    //public IEnumerable<string> Get()
+    //{
+    //    return new string[] { "value1", "value2" };
+    //}
+
+    //// GET api/values/5
+    //[HttpGet("{id}")]
+    //public string Get(int id)
+    //{
+    //    return "value";
+    //}
+
+    //// POST api/values
+    //[HttpPost]
+    //public void Post([FromBody]string value)
+    //{
+    //}
+
+    //// PUT api/values/5
+    //[HttpPut("{id}")]
+    //public void Put(int id, [FromBody]string value)
+    //{
+    //}
+
+    //// DELETE api/values/5
+    //[HttpDelete("{id}")]
+    //public void Delete(int id)
+    //{
+    //}
+  }
+}
--- src/HelloWorld_ASPNetMVC5_AJS/Startup.cs
+++ src/HelloWorld_ASPNetMVC5_AJS/Startup.cs
@@ -10,55 +10,20 @@ using Microsoft.Extensions.Logging;
 
 namespace HelloWorld_ASPNetMVC5_AJS
 {
-    public class Startup
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
     {
-        public Startup(IHostingEnvironment env)
-        {
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; set; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseIISPlatformHandler();
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+      services.AddMvc();
     }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseMvc();
+    }
+
+
+    // Entry point for the application.
+    public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+  }
 }
