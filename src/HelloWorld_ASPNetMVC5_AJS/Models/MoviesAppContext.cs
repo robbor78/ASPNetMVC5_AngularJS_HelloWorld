7810aae84f6c74f9befa6eb590625a0bb80825d8
username <email@email.com>
2016-03-03 16:52:56 +0200

    angular views

---
+++ src/HelloWorld_ASPNetMVC5_AJS/Models/MoviesAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld_ASPNetMVC5_AJS.Models
+{
+  public class MoviesAppContext : DbContext
+  {
+
+    public DbSet<Movie> Movies { get; set; }
+
+  }
+}
